f4433c796caaeaf1ab7c6e875c060f95d101d71b
Roboticsmaster123 <90348050+Roboticsmaster123@users.noreply.github.com>
2024-12-06 15:07:26 -0500

    .

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
@@ -0,0 +1,38 @@
+
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+//shows what name appears on the control hub for the class (line below)
+@Autonomous (name = "AutoFirstRedFar")
+// defines class and lets me use code in the SDK (line below)
+public class AutoRed extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+        while(!opModeIsActive()) {
+
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hardware {
+    //rf = lf
+    //lf = rf
+    //rb = lb
+    //lb = rb
+    //public BNO055IMU gyro;
+    public DcMotor rf;
+    public DcMotor rb;
+    public DcMotor lf;
+    public DcMotor lb;
+    public DcMotor lift;
+    //public CRServo wheel1;
+    //public DcMotorEx liftExtender;
+    public Servo claw1;
+    //public Servo claw2;
+
+
+    private static Hardware myInstance = null;
+    public double maxSpeed = 1;
+
+    public Hardware() {}
+
+    public static Hardware getInstance() {
+        if (myInstance == null) {
+            myInstance = new Hardware();
+        }
+        return myInstance;
+    }
+
+    public void init(HardwareMap hwMap) {
+
+        lift = hwMap.get(DcMotor.class,"lift");
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //lift.setDirection(DcMotorSimple.Direction.REVERSE);
+        lift.setPower(0);
+
+        rf = hwMap.get(DcMotor.class, "rf");
+        //rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setPower(0);
+
+        lf = hwMap.get(DcMotor.class, "lf");
+        //rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setPower(0);
+
+        rb = hwMap.get(DcMotor.class, "rb");
+        //rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setPower(0);
+
+        lb = hwMap.get(DcMotor.class, "lb");
+        //rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setPower(0);
+
+    }
+
+    public void setPower(double fr, double br, double fl, double bl) {
+        rf.setPower(Range.clip(fr, -maxSpeed, maxSpeed));
+        rb.setPower(Range.clip(br, -maxSpeed, maxSpeed));
+        lf.setPower(Range.clip(fl, -maxSpeed, maxSpeed));
+        lb.setPower(Range.clip(bl, -maxSpeed, maxSpeed));
+    }
+
+    public void liftsetPower(double apow) {
+        lift.setPower(apow);
+    }
+
+    public void setclaw1Pos(double pos) {
+        claw1.setPosition(pos);
+    }
+
+}
